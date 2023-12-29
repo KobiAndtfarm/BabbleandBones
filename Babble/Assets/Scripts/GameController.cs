5fae1ad0f472fa60978484fde046c86dd093f0bc
Ivorygriffin <s210559@students.aie.edu.au>
2023-12-29 15:03:20 +1030

    added skeleton of game controller +timer

--- Babble/Assets/Scripts/GameController.cs
+++ Babble/Assets/Scripts/GameController.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timeTillMorning;
+    private bool partOne, partTwo, partThree;
+    public GameObject clueCard;
+    private bool gameOver;
+    public TMP_Text overlayText;
+    private bool gateUnlocked;
 
-    // Update is called once per frame
+
+    public void Start()
+    {
+        partOne = true;
+    }
     void Update()
     {
-        
+        timeTillMorning -= Time.deltaTime;
+        CheckTimer();
+
+        if (partOne)
+        {
+            PartOne();
+        }
+        if (partTwo)
+        {
+            PartTwo();
+        }
+        if (partThree)
+        {
+            PartThree();
+        }
+  
+
+    }
+    public void CheckTimer()
+    {
+        if(timeTillMorning <= 0)
+        {
+            gameOver = true;
+        }
+    }
+    public void PartOne()
+    {
+        clueCard.SetActive(true);
+    } 
+    public void PartTwo()
+    {
+        clueCard.SetActive(true);
+    } 
+    public void PartThree()
+    {
+        overlayText.enabled = true;
+        overlayText.text = "You have completed the key, head to the graveyard";
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            overlayText.enabled = false;
+        }
+        gateUnlocked = true;
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("game over");
     }
 }
